5eb8f6f022d068621fca9e1825ab16008105db42
KimKimJungyeon <39543001+KimKimJungyeon@users.noreply.github.com>
2018-11-30 22:33:26 +0900

    코드 1차 수정

--- putCard.py
+++ putCard.py
@@ -3,53 +3,73 @@ class PutCard:
 
     def __init__(self, cardlist):
         self.HandCard = cardlist
-        self.count = 0
         self.putCount = 0
-        self.cardList= [1, 1, 100, 100]
+        self.downCardList = [1, 1, 100, 100]
 
-    def equalCard(self,card):
-        if card in self.HandCard:
-            return True
-        else:
-            return False
+    # def equalCard(self,card):
+    #     if card in self.HandCard:
+    #         return True
+    #     else:
+    #         return False
 
-    def basicCount(self):
+    def getDownCardList(self):
+        return self.downCardList
+
+    def resetPutCount(self):
         self.putCount = 0
 
-    def countReturn(self):
+    def getPutCount(self):
         return self.putCount
+
 
     def displayCard(self):
         cardlist = ''
         for i in range(4):
-            cardlist += (str(self.cardList[i])+' ')
+            cardlist += (str(self.downCardList[i]) + ' ')
         return cardlist
+
 
     def guess(self, cardlist, deckNum):
         count = 0
 
-        for i in range(len(cardlist)):
-            if cardlist[i] == '':
-                continue
-            else:
-                c = int(cardlist[i])
+        OneToHun1 = self.downCardList[0]
+        OneToHun2 = self.downCardList[1]
+        HunToOne1 = self.downCardList[2]
+        HunToOne2 = self.downCardList[3]
+	cardlist = sorted(cardlist)
 
-            if c + 10 == self.cardList[0] or c > self.cardList[0]:
-                count += 1
-            elif c + 10 == self.cardList[1] or c > self.cardList[1]:
-                count += 1
-            elif c - 10 == self.cardList[2] or c < self.cardList[2]:
-                count += 1
-            elif c - 10 == self.cardList[3] or c < self.cardList[3]:
-                count += 1
-            elif (c - 10) in cardlist:
-                count += 1
-            elif (c + 10) in cardlist:
-                count += 1
+        for i in range(len(cardlist)):
+            for j in range(len(cardlist)):
+                if cardlist[j] == '':
+                    continue
+                else:
+                    c = int(cardlist[j])
+
+                if c + 10 == OneToHun1 or c > OneToHun1:
+                    OneToHun1 = c
+                    count += 1
+                    break
+                elif c + 10 == OneToHun2 or c > OneToHun2:
+                    OneToHun2 = c
+                    count += 1
+                    break
+                elif c - 10 == HunToOne1 or c < HunToOne1:
+                    HunToOne1= c
+                    count += 1
+                    break
+                elif c - 10 == HunToOne2 or c < HunToOne2:
+                    HunToOne2 = c
+                    count += 1
+                    break
+                #elif (c - 10) in cardlist:
+                #    count += 1
+                #elif (c + 10) in cardlist:
+                #    count += 1
 
 
         # print(self.cardList[0] , self.cardList[1], self.cardList[2], self.cardList[3])
         # print(count)
+        print(count)
 
         if count < 2 and deckNum > 0:
             return False
@@ -62,16 +82,13 @@ class PutCard:
     def checkPut(self, downNum, holdNum, label):
         if label < 2:
             if downNum < holdNum or downNum == holdNum + 10:
-                self.cardList[label] = holdNum
+                self.downCardList[label] = holdNum
                 self.putCount +=1
-                self.count+=1
                 return True
         else:
             if downNum > holdNum or downNum == holdNum-10:
-                self.cardList[label] = holdNum
-                self.putCount+=1
-                self.count +=1
+                self.downCardList[label] = holdNum
+                self.putCount +=1
                 return True
-
         return False
 
